196152568e8dd67667b2cc4c505a4102bf8116f1
Muhammadullo <mnastulloev@gmail.com>
2023-11-28 19:29:12 +0500

    first commit

---
+++ js/api.js
@@ -0,0 +1,37 @@
+import { get } from "./dom.js";
+
+const API = "http://localhost:3000/data"
+
+
+// __________________________________
+async function getData() {
+    try {
+        let { data } = await axios.get(API)
+        console.log(data);
+        get(data)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// ____________________________________
+
+async function add(newUser) {
+    try {
+        let { data } = await axios.post(API, newUser)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// edit_____________________________
+async function editUser(id, elem) {
+    try {
+        let { data } = await axios.put(`${API}/${id}`, elem)
+        getData()
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export { getData, add , editUser}
---
+++ js/dom.js
@@ -0,0 +1,96 @@
+import { add, editUser } from "./api.js"
+
+let box = document.querySelector(".box")
+let form = document.querySelector(".form")
+let dialogEdit = document.querySelector(".dialogEdit")
+let formEdit = document.querySelector(".formEdit")
+let editPho = document.querySelector(".editPho")
+
+
+// _____________________________________add
+form["file"].onchange = (event) => {
+    let file = event.target.files[0]
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    form.onsubmit = async (event) => {
+        event.preventDefault()
+
+        let newUser = {
+            fullname: form["fullname"].value,
+            phone: form["phone"].value,
+            avatar: reader.result
+        }
+        add(newUser)
+    }
+
+}
+
+
+// ______________________________________________________
+// edit
+
+
+function editFun(element) {
+    dialogEdit.showModal()
+
+    editPho.src = element.avatar
+    formEdit["nameEdit"].value = element.fullname
+    formEdit["phoneEdit"].value = element.phone
+
+    formEdit["fileEdit"].onchange = (e) => {
+        let file = e.target.files[0]
+        let reader = new FileReader()
+        reader.readAsDataURL(file)
+        formEdit.onsubmit = (event) => {
+            event.preventDefault()
+            let newEditUser = {
+                fullname: formEdit["nameEdit"].value,
+                avatar: reader.result,
+                phone: formEdit["phoneEdit"].value,
+            }
+            editUser(element.id, newEditUser)
+            dialogEdit.close()
+        }
+    }
+
+}
+
+
+
+// ________________________________________________________
+function get(newData) {
+    newData.forEach(element => {
+
+        let avatar = document.createElement("img")
+        avatar.src = element.avatar
+        avatar.classList.add("avatar")
+
+        let fullname = document.createElement("h1")
+        fullname.innerHTML = element.fullname
+
+        let phone = document.createElement("h3")
+        phone.innerHTML = element.phone
+
+
+        let btnEdit = document.createElement("button")
+        btnEdit.innerHTML = "Edit"
+        btnEdit.onclick = () => {
+            editFun(element)
+        }
+
+        let btnDel = document.createElement("button")
+        btnDel.innerHTML = "Delete"
+
+        let card = document.createElement("div")
+        card.append(avatar, fullname, phone, btnEdit, btnDel)
+        card.classList.add("card")
+
+        box.append(card)
+    });
+}
+
+
+
+
+export { get }
